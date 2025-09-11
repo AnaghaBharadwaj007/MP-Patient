7ce39efb4bbf87ffd57569bf2425671b800ff6ae
anaghabharadwaj <anaghabharadwaj621@gmail.com>
2025-09-11 22:29:32 +0530

    Initialized ReactNative and NativewindCSS

---
+++ patientApp/app/(tabs)/explore.jsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function TabTwoScreen() {
+  return (
+    <SafeAreaView>
+      <Text className="bg-cyan-300 font-bold ">This is explore page</Text>
+    </SafeAreaView>
+  );
+}
